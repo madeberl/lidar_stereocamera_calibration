09ae8c2ae59055ee89ebc289f640c654fbdd1456
mdeberling <matthias.deberling@gmx.de>
2020-09-23 19:44:07 +0200

    Added Distance function, but threshold have to be adjusted

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def convert_kitti_bin_to_pcd(bin, name):
         byte = f.read(size_float * 4)
         while byte:
             x, y, z, intensity = struct.unpack("ffff", byte)
-            list_pcd.append([x, y, z])
+            if 4 <= x <= 30 and -5 <= y <= 20:
+                list_pcd.append([x, y, z])
+            # list_pcd.append([-y, z, -x])
             byte = f.read(size_float * 4)
     np_pcd = np.asarray(list_pcd)
     pcd = o3d.geometry.PointCloud()
@@ -22,20 +24,19 @@ def convert_kitti_bin_to_pcd(bin, name):
 def compute_distance(data, data_person):
     dists = data.compute_point_cloud_distance(data_person)
     dists = np.asarray(dists)
-    ind = np.where(dists > 0.01)[0]
+    ind = np.where(dists < 0.4)[0]
     data_without_person = data.select_by_index(ind)
     return data_without_person
 
 
 if __name__ == "__main__":
-    # file = "2011_09_26_drive_0001_sync/velodyne_points/data/0000000000.bin"
     file = "no_person.bin"
     file_person = "person.bin"
     name = file.split(".")
     name2 = file_person.split(".")
     pcd = convert_kitti_bin_to_pcd(file, name[0])
     pcd_person = convert_kitti_bin_to_pcd(file_person, name2[0])
-    # distance = compute_distance(pcd, pcd_person)
-    o3d.visualization.draw_geometries([pcd])
-    # o3d.visualization.draw_geometries([pcd])
+    distance = compute_distance(pcd, pcd_person)
+    o3d.io.write_point_cloud("distance.ply", distance)
+    # o3d.visualization.draw_geometries([distance])
     # o3d.visualization.draw_geometries([pcd_person])
