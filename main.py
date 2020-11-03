8e4f04440e15e4df902a6d1c4efa3a428707fed3
mdeberling <matthias.deberling@gmx.de>
2020-11-03 15:44:00 +0100

    First try with pylones, added file chooser

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+from tkinter.filedialog import askopenfilename
+from tkinter import Tk
 import numpy as np
 import open3d as o3d
 import struct
@@ -11,36 +14,40 @@ def convert_kitti_bin_to_pcd(bin, name):
         while byte:
             x, y, z, intensity = struct.unpack("ffff", byte)
             if 4 <= x <= 30 and -5 <= y <= 5:
-                # list_pcd.append([x, y, z])
-                list_pcd.append([-y, z, -x])
+                list_pcd.append([x, y, z])
+                # list_pcd.append([-y, z, -x])
             byte = f.read(size_float * 4)
     np_pcd = np.asarray(list_pcd)
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(np_pcd)
-    # o3d.visualization.draw_geometries([pcd], height=800, width=800)
+    o3d.visualization.draw_geometries([pcd], height=800, width=800)
     # o3d.io.write_point_cloud("data/" + name + ".ply", pcd)
     return pcd
 
 
-def compute_distance(data, data_person):
+def remove_points(file):
+    test = file
+
+
+def compute_distance(data, data_object, name):
     threshold = ([0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8])
     i = 0.1
     # for i in threshold:
-    dists = data_person.compute_point_cloud_distance(data)
+    dists = data_object.compute_point_cloud_distance(data)
     dists = np.asarray(dists)
     ind = np.where(dists > i)[0]
-    data_without_person = data_person.select_by_index(ind)
+    object = data_object.select_by_index(ind)
 
-    inlier_cloud = radius_outlier(data_without_person)
-    o3d.io.write_point_cloud("data/distance_radius_" + str(i) + ".ply", inlier_cloud)
-    inlier_cloud = statistical_outlier(data_without_person)
-    o3d.io.write_point_cloud("data/distance_statistical_" + str(i) + ".ply", inlier_cloud)
+    inlier_cloud = radius_outlier(object)
+    o3d.io.write_point_cloud("ply_data/cones/distance_radius_" + name + ".ply", inlier_cloud)
+    inlier_cloud = statistical_outlier(object)
+    o3d.io.write_point_cloud("ply_data/cones/distance_statistical_" + name + ".ply", inlier_cloud)
 
     return inlier_cloud
 
 
 def radius_outlier(cloud):
-    cl, ind = cloud.remove_radius_outlier(nb_points=16, radius=0.4)
+    cl, ind = cloud.remove_radius_outlier(nb_points=16, radius=0.8)
     inlier_cloud = cloud.select_by_index(ind)
     display_inlier_outlier(cloud, ind, "radius")
     bounding_box(inlier_cloud, "radius")
@@ -57,12 +64,12 @@ def statistical_outlier(cloud):
 
 def bounding_box(human, string):
     human = human.remove_non_finite_points()
-    #aligned = human.get_axis_aligned_bounding_box()
-    #aligned.color = (1, 0, 0)
+    aligned = human.get_axis_aligned_bounding_box()
+    aligned.color = (1, 0, 0)
     oriented = human.get_oriented_bounding_box()
     oriented.color = (0, 1, 0)
     # custom_draw_geometry_with_rotation(human, aligned, oriented)
-    o3d.visualization.draw_geometries([human, oriented], window_name=string,
+    o3d.visualization.draw_geometries([human, aligned, oriented], window_name=string,
                                       height=800, width=800)
 
 
@@ -81,7 +88,7 @@ def display_inlier_outlier(cloud, ind, string):
 def custom_draw_geometry_with_rotation(pcd, aligned, oriented):
     def rotate_view(vis):
         ctr = vis.get_view_control()
-        ctr.rotate(5.0, 0.0)
+        ctr.rotate(0.0, 0.0)
         return False
 
     o3d.visualization.draw_geometries_with_animation_callback([pcd, aligned, oriented],
@@ -89,12 +96,29 @@ def custom_draw_geometry_with_rotation(pcd, aligned, oriented):
 
 
 if __name__ == "__main__":
-    file = "no_person2.bin"
-    file_person = "person2.bin"
-    name = file.split(".")
-    name2 = file_person.split(".")
-    pcd = convert_kitti_bin_to_pcd(file, name[0])
-    pcd_person = convert_kitti_bin_to_pcd(file_person, name2[0])
-    distance = compute_distance(pcd, pcd_person)
+    Tk().withdraw()
+    # file = askopenfilename(initialdir=os.getcwd(), title="File without object")
+    file = "cone_data/without_cone_500.pcd"
+    file_object = askopenfilename(initialdir=os.getcwd(), title="File with object")
+    name = file.split("/")
+    name = name[1].split(".")
+    name2 = file_object.split("/")
+    name2 = name2[-1].split(".")
+    # test = convert_kitti_bin_to_pcd(file, name[1])
+    pcd = o3d.io.read_point_cloud(file)
+    # cropped_pcd = statistical_outlier(pcd, name[1])
+
+    pcd_pylone = o3d.io.read_point_cloud(file_object)
+    # cropped_pcd_object = statistical_outlier(pcd_pylone, name2[1])
+    # cropped_pcd_pylone = statistical_outlier(pcd_pylone, name2[1])
+    pylones = compute_distance(pcd, pcd_pylone, name2[0])
+    # o3d.io.write_point_cloud("ply_data/pylones.ply", pylones)
+    # file = "bin_data/no_person2.bin"
+    # file_person = "bin_data/person2.bin"
+    # file2 = "pcd_data/no_pylonen_2.pcd"
+    # file_pylonen = "pcd_data/pylonen_2.pcd"
+    # pcd = convert_kitti_bin_to_pcd(file, name[0])
+    # pcd_person = convert_kitti_bin_to_pcd(file_person, name2[0])
+    # distance = compute_distance(pcd, pcd_person)
     # bounding_box(distance)
-    # o3d.visualization.draw_geometries([distance], width=800, height=800)
+    # o3d.visualization.draw_geometries([pylones])
