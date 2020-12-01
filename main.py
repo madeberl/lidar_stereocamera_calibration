9c117563ad54b8fd4ed3148e059f3f135dc905ef
mdeberling <matthias.deberling@gmx.de>
2020-12-01 18:57:31 +0100

    Added normalised vectors

--- main.py
+++ main.py
@@ -21,12 +21,18 @@ def convert_kitti_bin_to_pcd(bin, name):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(np_pcd)
     o3d.visualization.draw_geometries([pcd], height=800, width=800)
-    # o3d.io.write_point_cloud("data/" + name + ".ply", pcd)
+    o3d.io.write_point_cloud("data/" + name + ".ply", pcd)
     return pcd
 
 
 def remove_points(file):
-    test = file
+    point = np.asarray(file.points)
+    point_new = point[(point[:, 0] > 0) & (point[:, 0] < 22)
+                      & (point[:, 1] > -1) & (point[:, 1] < 2)
+                      & (point[:, 2] > -0.6) & (point[:, 2] < 1)]
+    pcd_new = o3d.geometry.PointCloud()
+    pcd_new.points = o3d.utility.Vector3dVector(point_new)
+    return pcd_new
 
 
 def compute_distance(data, data_object, name):
@@ -37,28 +43,29 @@ def compute_distance(data, data_object, name):
     dists = np.asarray(dists)
     ind = np.where(dists > i)[0]
     object = data_object.select_by_index(ind)
-
-    inlier_cloud = radius_outlier(object)
-    o3d.io.write_point_cloud("ply_data/cones/distance_radius_" + name + ".ply", inlier_cloud)
+    o3d.visualization.draw_geometries([object], window_name=str(i),
+                                          height=800, width=800)
+    # object = object.voxel_down_sample(voxel_size=0.01)
+    # inlier_cloud = radius_outlier(object)
+    # o3d.io.write_point_cloud("ply_data/cones/distance_radius_" + name + ".ply", inlier_cloud)
     inlier_cloud = statistical_outlier(object)
-    o3d.io.write_point_cloud("ply_data/cones/distance_statistical_" + name + ".ply", inlier_cloud)
-
+    # o3d.io.write_point_cloud("ply_data/packet/distance_statistical_" + str(i) + ".ply", inlier_cloud)
     return inlier_cloud
 
 
 def radius_outlier(cloud):
-    cl, ind = cloud.remove_radius_outlier(nb_points=16, radius=0.8)
+    cl, ind = cloud.remove_radius_outlier(nb_points=15, radius=0.5)
     inlier_cloud = cloud.select_by_index(ind)
-    display_inlier_outlier(cloud, ind, "radius")
-    bounding_box(inlier_cloud, "radius")
+    # display_inlier_outlier(cloud, ind, "radius")
+    # bounding_box(inlier_cloud, "radius")
     return inlier_cloud
 
 
 def statistical_outlier(cloud):
-    cl, ind = cloud.remove_statistical_outlier(nb_neighbors=80, std_ratio=0.1)
+    cl, ind = cloud.remove_statistical_outlier(nb_neighbors=10, std_ratio=0.5)
     inlier_cloud = cloud.select_by_index(ind)
-    display_inlier_outlier(cloud, ind, "statistical")
-    bounding_box(inlier_cloud, "statistical")
+    # display_inlier_outlier(cloud, ind, "statistical")
+    # bounding_box(inlier_cloud, "statistical")
     return inlier_cloud
 
 
@@ -66,10 +73,10 @@ def bounding_box(human, string):
     human = human.remove_non_finite_points()
     aligned = human.get_axis_aligned_bounding_box()
     aligned.color = (1, 0, 0)
-    oriented = human.get_oriented_bounding_box()
-    oriented.color = (0, 1, 0)
+    # oriented = human.get_oriented_bounding_box()
+    # oriented.color = (0, 1, 0)
     # custom_draw_geometry_with_rotation(human, aligned, oriented)
-    o3d.visualization.draw_geometries([human, aligned, oriented], window_name=string,
+    o3d.visualization.draw_geometries([human, aligned], window_name=string,
                                       height=800, width=800)
 
 
@@ -95,30 +102,28 @@ def custom_draw_geometry_with_rotation(pcd, aligned, oriented):
                                                               rotate_view, height=800, width=800)
 
 
+def normal_estimation(downpcd):
+    downpcd.estimate_normals(
+        search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.5, max_nn=200))
+    return downpcd
+
+
 if __name__ == "__main__":
     Tk().withdraw()
     # file = askopenfilename(initialdir=os.getcwd(), title="File without object")
-    file = "cone_data/without_cone_500.pcd"
-    file_object = askopenfilename(initialdir=os.getcwd(), title="File with object")
-    name = file.split("/")
-    name = name[1].split(".")
+    file = "packet_data/test_data/empty_room.pcd"
+    # file_object = askopenfilename(initialdir=os.getcwd(), title="File with object")
+    file_object = "packet_data/test_data/packet_5m.pcd"
     name2 = file_object.split("/")
     name2 = name2[-1].split(".")
-    # test = convert_kitti_bin_to_pcd(file, name[1])
-    pcd = o3d.io.read_point_cloud(file)
-    # cropped_pcd = statistical_outlier(pcd, name[1])
 
-    pcd_pylone = o3d.io.read_point_cloud(file_object)
-    # cropped_pcd_object = statistical_outlier(pcd_pylone, name2[1])
-    # cropped_pcd_pylone = statistical_outlier(pcd_pylone, name2[1])
-    pylones = compute_distance(pcd, pcd_pylone, name2[0])
-    # o3d.io.write_point_cloud("ply_data/pylones.ply", pylones)
-    # file = "bin_data/no_person2.bin"
-    # file_person = "bin_data/person2.bin"
-    # file2 = "pcd_data/no_pylonen_2.pcd"
-    # file_pylonen = "pcd_data/pylonen_2.pcd"
-    # pcd = convert_kitti_bin_to_pcd(file, name[0])
-    # pcd_person = convert_kitti_bin_to_pcd(file_person, name2[0])
-    # distance = compute_distance(pcd, pcd_person)
-    # bounding_box(distance)
-    # o3d.visualization.draw_geometries([pylones])
+    pcd = o3d.io.read_point_cloud(file)
+    pcd_removed = remove_points(pcd)
+    pcd_object = o3d.io.read_point_cloud(file_object)
+    pcd_removed_object = remove_points(pcd_object)
+
+    object_isolated = compute_distance(pcd_removed, pcd_removed_object, "test")
+    normals_estimated = normal_estimation(object_isolated)
+    o3d.visualization.draw_geometries([normals_estimated], "Normale", height=800, width=800,
+                                      point_show_normal=True)
+    o3d.io.write_point_cloud("packet_data/test_data/packet_normalised.ply", normals_estimated)
